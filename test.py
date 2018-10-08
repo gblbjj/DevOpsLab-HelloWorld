35a13bc7fe40a215b64f3e7176a18eb3744acdd2
oliveira992gabriel <oliveira992gabriel@gmail.com>
2018-10-08 14:39:31 -0400

    update test.py

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "Escreva uma Mensagem para o Cabeçalho da Página.")
+        self.assertRegex(result.data.decode(), "Palmeiras não tem mundial...e em 2018 vai continuar sem !!!.")
 
 
 if __name__ == "__main__":
